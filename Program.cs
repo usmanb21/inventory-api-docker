3b347d481085cc4d3a8667633cfe73c1a3e334f6
Usman Butt <usmanbutt@Usmans-MacBook-Air.local>
2026-04-15 15:07:01 +0200

    fix: remove conflicting port binding

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://0.0.0.0:8080");
 // ======================
 // Services
 // ======================
